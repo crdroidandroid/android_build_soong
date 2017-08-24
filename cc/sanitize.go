8660419d74aa2b76510ee9e059b481c6771047c7
Daniel Micay <danielmicay@gmail.com>
2017-08-23 20:28:03 -0400

    use -fwrapv when signed overflow checking is off
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- cc/sanitize.go
+++ cc/sanitize.go
@@ -315,6 +315,7 @@ func (sanitize *sanitize) flags(ctx ModuleContext, flags Flags) Flags {
 		flags.LdFlags = append(flags.LdFlags, "-Wl,--exclude-libs,"+minimalRuntimeLib)
 	}
 	if !sanitize.Properties.SanitizerEnabled {
+		flags.CFlags = append(flags.CFlags, "-fwrapv")
 		return flags
 	}
 
@@ -354,6 +355,17 @@ func (sanitize *sanitize) flags(ctx ModuleContext, flags Flags) Flags {
 			)
 		}
 		sanitizers = append(sanitizers, sanitize.Properties.Sanitize.Misc_undefined...)
+	}
+
+	wrapv := true
+	for _, element := range sanitizers {
+		if (element == "undefined" || element == "integer" || element == "signed-integer-overflow") {
+			wrapv = false
+			break
+		}
+	}
+	if wrapv {
+		flags.CFlags = append(flags.CFlags, "-fwrapv")
 	}
 
 	if Bool(sanitize.Properties.Sanitize.Diag.Undefined) {
