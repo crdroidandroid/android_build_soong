a55d4ce804b3acc917d734cc296a21cf892c083c
Pranav Vashi <neobuddy89@gmail.com>
2019-09-15 23:56:44 +0530

    soong: Change to CR_VERSION
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -147,10 +147,10 @@ func dumpMakeVars(ctx Context, config Config, goals, vars []string, write_soong_
 var BannerVars = []string{
 	"PLATFORM_VERSION_CODENAME",
 	"PLATFORM_VERSION",
-	"LINEAGE_VERSION",
+	"CR_VERSION",
 	"PRODUCT_INCLUDE_TAGS",
 	"PRODUCT_SOURCE_ROOT_DIRS",
-	"TARGET_PRODUCT",
+	"TARGET_DEVICE",
 	"TARGET_BUILD_VARIANT",
 	"TARGET_BUILD_APPS",
 	"TARGET_BUILD_UNBUNDLED",
