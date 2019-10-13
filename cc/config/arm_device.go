4b5691b3d5129ab550b3c47c96f6ca211a4762d7
Jake Weinstein <jake@aospa.co>
2019-10-13 23:21:36 +0200

    soong: use -mcpu=cortex-a55 for kryo300 and kryo385
    Change-Id: I726a62bfa14029b575de1821fc0676c9bef229c8
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- cc/config/arm_device.go
+++ cc/config/arm_device.go
@@ -151,9 +151,18 @@ var (
 			// better solution comes around. See Bug 27340895
 			"-D__ARM_FEATURE_LPAE=1",
 		},
+                "kryo300": []string{
+                        // Use cortex-a55 because kryo300 is not supported in GCC/clang.
+                        "-mcpu=cortex-a55",
+                        // Fake an ARM compiler flag as these processors support LPAE which GCC/clang
+                        // don't advertise.
+                        // TODO This is a hack and we need to add it for each processor that supports LPAE until some
+                        // better solution comes around. See Bug 27340895
+                        "-D__ARM_FEATURE_LPAE=1",
+                },
 		"kryo385": []string{
-			// Use cortex-a53 because kryo385 is not supported in GCC/clang.
-			"-mcpu=cortex-a53",
+			// Use cortex-a55 because kryo385 is not supported in GCC/clang.
+			"-mcpu=cortex-a55",
 			// Fake an ARM compiler flag as these processors support LPAE which GCC/clang
 			// don't advertise.
 			// TODO This is a hack and we need to add it for each processor that supports LPAE until some
@@ -239,7 +248,8 @@ var (
 		"cortex-a76":     "${config.ArmClangCortexA55Cflags}",
 		"krait":          "${config.ArmClangKraitCflags}",
 		"kryo":           "${config.ArmClangKryoCflags}",
-		"kryo385":        "${config.ArmClangCortexA53Cflags}",
+		"kryo300":        "${config.ArmClangCortexA55Cflags}",
+		"kryo385":        "${config.ArmClangCortexA55Cflags}",
 		"exynos-m1":      "${config.ArmClangCortexA53Cflags}",
 		"exynos-m2":      "${config.ArmClangCortexA53Cflags}",
 	}
