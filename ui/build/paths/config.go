4c07aa5c0244105d348c28174be0caa2cdade808
Jyotiraditya <dreadnaught02@outlook.com>
2019-11-01 12:42:40 +0000

    Allow the flock for kernel builds
    requires for kernels which has wireguard
    and its common having kernels with wireguard. so allow it, what are you waiting for?
    
    Signed-off-by: Jyotiraditya <dreadnaught02@outlook.com>
    Change-Id: I4ef18f857c27df99b1f262ab8bd54e1d44549298

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -84,6 +84,7 @@ var Configuration = map[string]PathConfig{
 	"egrep":    Allowed,
 	"expr":     Allowed,
 	"find":     Allowed,
+	"flock":    Allowed,
 	"fuser":    Allowed,
 	"getopt":   Allowed,
 	"git":      Allowed,
