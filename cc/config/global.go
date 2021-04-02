syberia-project/platform_build_soong

soong: Add custom clang with polly support
- Setup for Clang 12, but can be overridden with:
   CUSTOM_CLANG_VERSION (Example: clang-benzo)
   CUSTOM_CLANG_SHORT_VERSION (Example: 12.0.0)

- Links polly enabled modules with libomp and -fopenmp

- Any new modules that fail to build with polly can be added
    to cc/cc.go but with the current release works now

Change-Id: Ie6b4b890de0b766ae102f34a58d6cd8b36f36a7f
Signed-off-by: Jason Edson <jaysonedson@gmail.com>

cc/config/global.go
@@ -32,6 +32,7 @@ var (
 		"-Wno-unused",
 		"-Winit-self",
 		"-Wpointer-arith",
+		"-Wno-inconsistent-missing-override",
 
 		// Make paths in deps files relative
 		"-no-canonical-prefixes",
@@ -97,6 +98,18 @@ var (
 			"-fuse-ld=lld",
 		}...)
 
+	pollyCflags = []string{
+		"-O3",
+		"-mllvm", "-polly",
+		"-mllvm", "-polly-parallel", "-lgomp",
+		"-mllvm", "-polly-omp-backend=LLVM",
+		"-mllvm", "-polly-opt-fusion=max",
+		"-mllvm", "-polly-position=after-loopopt",
+		"-mllvm", "-polly-run-dce",
+		"-mllvm", "-polly-scheduling=dynamic",
+		"-mllvm", "-polly-vectorizer=polly",
+	}
+
 	hostGlobalCflags = []string{}
 
 	hostGlobalCppflags = []string{}
@@ -123,13 +136,14 @@ var (
 	CppStdVersion             = "gnu++17"
 	ExperimentalCStdVersion   = "gnu11"
 	ExperimentalCppStdVersion = "gnu++2a"
+	Polly                     = true
 
 	NdkMaxPrebuiltVersionInt = 27
 
 	// prebuilts/clang default settings.
 	ClangDefaultBase         = "prebuilts/clang/host"
-	ClangDefaultVersion      = "clang-r383902b"
-	ClangDefaultShortVersion = "11.0.2"
+	ClangDefaultVersion      = "clang-benzo"
+	ClangDefaultShortVersion = "12.0.0"
 
 	// Directories with warnings from Android.bp files.
 	WarningAllowedProjects = []string{
@@ -220,7 +234,7 @@ func init() {
 		return "${ClangDefaultBase}"
 	})
 	pctx.VariableFunc("ClangVersion", func(ctx android.PackageVarContext) string {
-		if override := ctx.Config().Getenv("LLVM_PREBUILTS_VERSION"); override != "" {
+		if override := ctx.Config().CustomClangVersion(); override != "" {
 			return override
 		}
 		return ClangDefaultVersion
@@ -229,12 +243,14 @@ func init() {
 	pctx.StaticVariable("ClangBin", "${ClangPath}/bin")
 
 	pctx.VariableFunc("ClangShortVersion", func(ctx android.PackageVarContext) string {
-		if override := ctx.Config().Getenv("LLVM_RELEASE_VERSION"); override != "" {
+		if override := ctx.Config().CustomClangShortVersion(); override != "" {
 			return override
 		}
 		return ClangDefaultShortVersion
 	})
 	pctx.StaticVariable("ClangAsanLibDir", "${ClangBase}/linux-x86/${ClangVersion}/lib64/clang/${ClangShortVersion}/lib/linux")
+
+	pctx.StaticVariable("PollyFlags", strings.Join(pollyCflags, " "))
 
 	// These are tied to the version of LLVM directly in external/llvm, so they might trail the host prebuilts
 	// being used for the rest of the build process.

crdroidandroid/android_build_soong

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

cc/config/global.go
@@ -32,6 +32,7 @@ var (
 		"-Wno-unused",
 		"-Winit-self",
 		"-Wpointer-arith",
+		"-Wno-inconsistent-missing-override",
 
 		// Make paths in deps files relative
 		"-no-canonical-prefixes",
@@ -97,6 +98,18 @@ var (
 			"-fuse-ld=lld",
 		}...)
 
+	pollyCflags = []string{
+		"-O3",
+		"-mllvm", "-polly",
+		"-mllvm", "-polly-parallel", "-lgomp",
+		"-mllvm", "-polly-omp-backend=LLVM",
+		"-mllvm", "-polly-opt-fusion=max",
+		"-mllvm", "-polly-position=after-loopopt",
+		"-mllvm", "-polly-run-dce",
+		"-mllvm", "-polly-scheduling=dynamic",
+		"-mllvm", "-polly-vectorizer=polly",
+	}
+
 	hostGlobalCflags = []string{}
 
 	hostGlobalCppflags = []string{}
@@ -123,13 +136,14 @@ var (
 	CppStdVersion             = "gnu++17"
 	ExperimentalCStdVersion   = "gnu11"
 	ExperimentalCppStdVersion = "gnu++2a"
+	Polly                     = true
 
 	NdkMaxPrebuiltVersionInt = 27
 
 	// prebuilts/clang default settings.
 	ClangDefaultBase         = "prebuilts/clang/host"
-	ClangDefaultVersion      = "clang-r383902b1"
-	ClangDefaultShortVersion = "11.0.2"
+	ClangDefaultVersion      = "clang-r417554"
+	ClangDefaultShortVersion = "13.0.0"
 
 	// Directories with warnings from Android.bp files.
 	WarningAllowedProjects = []string{
@@ -220,7 +234,7 @@ func init() {
 		return "${ClangDefaultBase}"
 	})
 	pctx.VariableFunc("ClangVersion", func(ctx android.PackageVarContext) string {
-		if override := ctx.Config().Getenv("LLVM_PREBUILTS_VERSION"); override != "" {
+		if override := ctx.Config().CustomClangVersion(); override != "" {
 			return override
 		}
 		return ClangDefaultVersion
@@ -229,12 +243,14 @@ func init() {
 	pctx.StaticVariable("ClangBin", "${ClangPath}/bin")
 
 	pctx.VariableFunc("ClangShortVersion", func(ctx android.PackageVarContext) string {
-		if override := ctx.Config().Getenv("LLVM_RELEASE_VERSION"); override != "" {
+		if override := ctx.Config().CustomClangShortVersion(); override != "" {
 			return override
 		}
 		return ClangDefaultShortVersion
 	})
 	pctx.StaticVariable("ClangAsanLibDir", "${ClangBase}/linux-x86/${ClangVersion}/lib64/clang/${ClangShortVersion}/lib/linux")
+
+	pctx.StaticVariable("PollyFlags", strings.Join(pollyCflags, " "))
 
 	// These are tied to the version of LLVM directly in external/llvm, so they might trail the host prebuilts
 	// being used for the rest of the build process.