c2426d27288710ae1bdfdf9125f699cba5aee2c1
Jake Weinstein <jake@aospa.co>
2023-08-07 14:51:00 +0900

    Use -march=armv9-a+nosve for Armv9-A arch variant
    SVE is not supported by current compilers, but
    other Armv9-A features can be enabled.
    
    Test: llvm-objdump -d
    Change-Id: I99db407eb96d82d189378fd5abd4cc80ab802f20
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- cc/config/arm64_device.go
+++ cc/config/arm64_device.go
@@ -44,7 +44,7 @@ var (
 		// On ARMv9 and later, Pointer Authentication Codes (PAC) are mandatory,
 		// so -fstack-protector is unnecessary.
 		"armv9-a": []string{
-			"-march=armv8.2-a+dotprod",
+			"-march=armv9-a+nosve",
 			"-mbranch-protection=standard",
 			"-fno-stack-protector",
 		},
