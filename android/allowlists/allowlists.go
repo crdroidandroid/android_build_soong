31d64763d2b991e7b8705f6df357d5f4f54b9047
John Galt <johngaltfirstrun@gmail.com>
2023-12-01 13:58:53 -0500

    allowlist: remove server_configurable_flags
    Amends cfbe947aed9a991fd204704f2a35741273108357 since this module is now
    afdo enabled
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- android/allowlists/allowlists.go
+++ android/allowlists/allowlists.go
@@ -529,7 +529,6 @@ var (
 		"packagemanager_aidl_interface",
 		"philox_random",
 		"philox_random_headers",
-		"server_configurable_flags",
 		"service-permission-streaming-proto-sources",
 		"statslog_neuralnetworks.cpp",
 		"statslog_neuralnetworks.h",
