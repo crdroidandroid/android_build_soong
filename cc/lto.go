f2653f0864a689c87fbe24a07e88a3d77f46149d
Pranav Vashi <neobuddy89@gmail.com>
2024-05-24 21:29:21 +0530

    Revert "Enable full LTO optimization by default"
    This reverts commit 6e60ff8df04fded1ac2b47ff85896cfa3f4619bb.

--- cc/lto.go
+++ cc/lto.go
@@ -56,9 +56,6 @@ type LTOProperties struct {
 
 	// Use -fwhole-program-vtables cflag.
 	Whole_program_vtables *bool
-
-	// Use --lto-O0 flag.
-	Lto_O0 *bool
 }
 
 type lto struct {
@@ -116,8 +113,12 @@ func (lto *lto) flags(ctx BaseModuleContext, flags Flags) Flags {
 		var ltoCOnlyFlags []string
 		var ltoLdFlags []string
 
-		// Do not perform costly LTO optimizations for Eng builds.
-		if Bool(lto.Properties.Lto_O0) || ctx.Config().Eng() {
+		// The module did not explicitly turn on LTO. Only leverage LTO's
+		// better dead code elimination and CFG simplification, but do
+		// not perform costly optimizations for a balance between compile
+		// time, binary size and performance.
+		// Apply the same for Eng builds as well.
+		if !lto.ThinLTO() || ctx.Config().Eng() {
 			ltoLdFlags = append(ltoLdFlags, "-Wl,--lto-O0")
 		} else {
 			ltoLdFlags = append(ltoLdFlags,"-Wl,--lto-O3")
