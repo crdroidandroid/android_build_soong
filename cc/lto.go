2b67f8fd6ed1234ae61b1afe230aaed7ce07bb89
Pranav Vashi <neobuddy89@gmail.com>
2024-05-24 21:29:50 +0530

    Partially Revert "cc: enable Polly globally"
    This reverts commit 8c0bc52e7dfd1205e529e5d873656676d18ff547.

--- cc/lto.go
+++ cc/lto.go
@@ -125,20 +125,6 @@ func (lto *lto) flags(ctx BaseModuleContext, flags Flags) Flags {
 			ltoCOnlyFlags = append(ltoCOnlyFlags, "-O3")
 		}
 
-		// Enable Polly globally
-		ltoCOnlyFlags = append(ltoCOnlyFlags, "-mllvm -polly")
-		ltoCOnlyFlags = append(ltoCOnlyFlags, "-mllvm -polly-parallel")
-		ltoCOnlyFlags = append(ltoCOnlyFlags, "-mllvm -polly-ast-use-context")
-		ltoCOnlyFlags = append(ltoCOnlyFlags, "-mllvm -polly-invariant-load-hoisting")
-		ltoCOnlyFlags = append(ltoCOnlyFlags, "-mllvm -polly-run-inliner")
-		ltoCOnlyFlags = append(ltoCOnlyFlags, "-mllvm -polly-loopfusion-greedy=1")
-		ltoCOnlyFlags = append(ltoCOnlyFlags, "-mllvm -polly-reschedule=1")
-		ltoCOnlyFlags = append(ltoCOnlyFlags, "-mllvm -polly-postopts=1")
-		ltoCOnlyFlags = append(ltoCOnlyFlags, "-mllvm -polly-omp-backend=LLVM")
-		ltoCOnlyFlags = append(ltoCOnlyFlags, "-mllvm -polly-scheduling=dynamic")
-		ltoCOnlyFlags = append(ltoCOnlyFlags, "-mllvm -polly-scheduling-chunksize=1")
-		ltoCOnlyFlags = append(ltoCOnlyFlags, "-mllvm -polly-vectorizer=stripmine")
-
 		if Bool(lto.Properties.Whole_program_vtables) {
 			ltoCFlags = append(ltoCFlags, "-fwhole-program-vtables")
 		}
