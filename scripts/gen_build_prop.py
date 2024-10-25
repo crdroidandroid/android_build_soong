5e45584bdcaab550158d548f82b85e854066392a
Pranav Vashi <neobuddy89@gmail.com>
2024-10-25 17:52:40 +0530

    soong: Set ro.crdroid.device
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -246,6 +246,7 @@ def generate_build_info(args):
   print(f"ro.build.flavor={config['BuildFlavor']}")
 
   print(f"ro.lineage.device={config['LineageDevice']}")
+  print(f"ro.crdroid.device={config['LineageDevice']}")
 
   # These values are deprecated, use "ro.product.cpu.abilist"
   # instead (see below).
