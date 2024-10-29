27f56f763b3c372223ca937428e5922bbf02d821
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-29 17:55:37 +0800

    gen_build_prop: Always return release-keys
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -40,10 +40,7 @@ def get_build_flavor(product_config):
   return build_flavor
 
 def get_build_keys(product_config):
-  default_cert = product_config.get("DefaultAppCertificate", "")
-  if default_cert == "" or default_cert == os.path.join(TEST_KEY_DIR, "testKey"):
-    return "test-keys"
-  return "dev-keys"
+  return "release-keys"
 
 def override_config(config):
   if "PRODUCT_BUILD_PROP_OVERRIDES" in config:
