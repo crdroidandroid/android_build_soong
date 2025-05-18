af67f6b827fa01ec5172d7e3dedb4fd305708110
John Galt <johngaltfirstrun@gmail.com>
2025-05-18 19:24:06 -0400

    arm64_device: armv9-a: add additional march flags for all supported armv9-a platforms
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- cc/config/arm64_device.go
+++ cc/config/arm64_device.go
@@ -34,7 +34,7 @@ var (
 		"armv8-2a-dotprod":   {"-march=armv8.2-a+lse+fp16+dotprod"},
 		"armv8-5a":           {"-march=armv8.5-a"},
 		"armv8-7a":           {"-march=armv8.7-a"},
-		"armv9-a":            {"-march=armv9-a+crypto+nosve"},
+		"armv9-a":            {"-march=armv9-a+crypto+nosve+dotprod+fp16+i8mm"},
 		"armv9-2a":           {"-march=armv9.2-a"},
 		"armv9-3a":           {"-march=armv9.3-a"},
 		"armv9-4a":           {"-march=armv9.4-a"},
